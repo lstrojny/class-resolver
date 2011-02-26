53818b077e49fc31596c27dd01b1da8bd6d122ac
Lars Strojny <lars@strojny.net>
2011-02-26 02:00:47 +0100

    Infra

--- config.m4
+++ config.m4
@@ -1,30 +1,9 @@
-dnl
-dnl $ Id: $
-dnl
-
-PHP_ARG_ENABLE(classnameresolver, whether to enable classnameresolver functions,
-[  --enable-classnameresolver         Enable classnameresolver support])
+PHP_ARG_ENABLE(classnameresolver, for classname resolver support,
+[  --enable-classname-resolver          Include classname resolver support])
 
 if test "$PHP_CLASSNAMERESOLVER" != "no"; then
-  export OLD_CPPFLAGS="$CPPFLAGS"
-  export CPPFLAGS="$CPPFLAGS $INCLUDES -DHAVE_CLASSNAMERESOLVER"
-
-  AC_MSG_CHECKING(PHP version)
-  AC_TRY_COMPILE([#include <php_version.h>], [
-#if PHP_VERSION_ID < 40000
-#error  this extension requires at least PHP version 4.0.0
-#endif
-],
-[AC_MSG_RESULT(ok)],
-[AC_MSG_ERROR([need at least PHP 4.0.0])])
-
-  export CPPFLAGS="$OLD_CPPFLAGS"
-
-
   PHP_SUBST(CLASSNAMERESOLVER_SHARED_LIBADD)
-  AC_DEFINE(HAVE_CLASSNAMERESOLVER, 1, [ ])
 
-  PHP_NEW_EXTENSION(classnameresolver, classnameresolver.c , $ext_shared)
-
+  PHP_NEW_EXTENSION(classnameresolver, classnameresolver.c resolver.c, $ext_shared)
+  CFLAGS="$CFLAGS -Wall -g"
 fi
-
--- php_classnameresolver.h
+++ php_classnameresolver.h
@@ -1,87 +1,42 @@
-/*
-   +----------------------------------------------------------------------+
-   | unknown license:                                                      |
-   +----------------------------------------------------------------------+
-   +----------------------------------------------------------------------+
-*/
-
-/* $ Id: $ */ 
-
 #ifndef PHP_CLASSNAMERESOLVER_H
 #define PHP_CLASSNAMERESOLVER_H
-
-#ifdef  __cplusplus
-extern "C" {
-#endif
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
-#include <php.h>
-
-#ifdef HAVE_CLASSNAMERESOLVER
-#define PHP_CLASSNAMERESOLVER_VERSION "0.0.1dev"
-
-
-#include <php_ini.h>
-#include <SAPI.h>
-#include <ext/standard/info.h>
-#include <Zend/zend_extensions.h>
-#ifdef  __cplusplus
-} // extern "C" 
-#endif
-#ifdef  __cplusplus
-extern "C" {
-#endif
+#include "php.h"
 
 extern zend_module_entry classnameresolver_module_entry;
-#define phpext_classnameresolver_ptr &classnameresolver_module_entry
 
-#ifdef PHP_WIN32
-#define PHP_CLASSNAMERESOLVER_API __declspec(dllexport)
-#else
-#define PHP_CLASSNAMERESOLVER_API
-#endif
+#define phpext_oauth_ptr &classnameresolver_module_entry
 
 PHP_MINIT_FUNCTION(classnameresolver);
 PHP_MSHUTDOWN_FUNCTION(classnameresolver);
-PHP_RINIT_FUNCTION(classnameresolver);
-PHP_RSHUTDOWN_FUNCTION(classnameresolver);
 PHP_MINFO_FUNCTION(classnameresolver);
 
+
 #ifdef ZTS
-#include "TSRM.h"
+#define CLASSNAMERESOLVER(v) TSRMG(classnameresolver_globals_id, zend_classnameresolver_globals *, v)
+#else
+#define CLASSNAMERESOLVER(v) (classnameresolver_globals.v)
 #endif
 
-#define FREE_RESOURCE(resource) zend_list_delete(Z_LVAL_P(resource))
-
-#define PROP_GET_LONG(name)    Z_LVAL_P(zend_read_property(_this_ce, _this_zval, #name, strlen(#name), 1 TSRMLS_CC))
-#define PROP_SET_LONG(name, l) zend_update_property_long(_this_ce, _this_zval, #name, strlen(#name), l TSRMLS_CC)
-
-#define PROP_GET_DOUBLE(name)    Z_DVAL_P(zend_read_property(_this_ce, _this_zval, #name, strlen(#name), 1 TSRMLS_CC))
-#define PROP_SET_DOUBLE(name, d) zend_update_property_double(_this_ce, _this_zval, #name, strlen(#name), d TSRMLS_CC)
-
-#define PROP_GET_STRING(name)    Z_STRVAL_P(zend_read_property(_this_ce, _this_zval, #name, strlen(#name), 1 TSRMLS_CC))
-#define PROP_GET_STRLEN(name)    Z_STRLEN_P(zend_read_property(_this_ce, _this_zval, #name, strlen(#name), 1 TSRMLS_CC))
-#define PROP_SET_STRING(name, s) zend_update_property_string(_this_ce, _this_zval, #name, strlen(#name), s TSRMLS_CC)
-#define PROP_SET_STRINGL(name, s, l) zend_update_property_stringl(_this_ce, _this_zval, #name, strlen(#name), s, l TSRMLS_CC)
-
-
-#ifdef  __cplusplus
-} // extern "C" 
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
 #endif
 
-#endif /* PHP_HAVE_CLASSNAMERESOLVER */
+#define CLASSNAMERESOLVER_EXT_VERSION "0.0.1"
 
-#endif /* PHP_CLASSNAMERESOLVER_H */
+#endif
 
-
-/*
- * Local variables:
- * tab-width: 4
+/**
+ * Local Variables:
  * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: t
  * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
+ * vim600: fdm=marker
+ * vim: noet sw=4 ts=4 noexpandtab
  */
