71633e19f583a327f51c13c498b378e63a7641f7
Lars Strojny <lars@strojny.net>
2011-02-28 10:25:53 +0100

    First prototype

--- config.m4
+++ config.m4
@@ -4,6 +4,6 @@ PHP_ARG_ENABLE(classnameresolver, for classname resolver support,
 if test "$PHP_CLASSNAMERESOLVER" != "no"; then
   PHP_SUBST(CLASSNAMERESOLVER_SHARED_LIBADD)
 
-  PHP_NEW_EXTENSION(classnameresolver, classnameresolver.c resolver.c, $ext_shared)
+  PHP_NEW_EXTENSION(classnameresolver, classnameresolver.c, $ext_shared)
   CFLAGS="$CFLAGS -Wall -g"
 fi
--- php_classnameresolver.h
+++ php_classnameresolver.h
@@ -1,20 +1,27 @@
 #ifndef PHP_CLASSNAMERESOLVER_H
 #define PHP_CLASSNAMERESOLVER_H
+#define CLASSNAMERESOLVER_VERSION "0.0.1"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
 #include "php.h"
+#include "zend_API.h"
+#include "zend_compile.h"
 
 extern zend_module_entry classnameresolver_module_entry;
+#define phpext_classnameresolver_ptr &classnameresolver_module_entry
 
-#define phpext_oauth_ptr &classnameresolver_module_entry
+PHP_METHOD(ClassnameResolver, registerResolver);
 
 PHP_MINIT_FUNCTION(classnameresolver);
 PHP_MSHUTDOWN_FUNCTION(classnameresolver);
 PHP_MINFO_FUNCTION(classnameresolver);
 
+void (*zend_do_begin_new_object_orig)(znode *new_token, znode *class_type TSRMLS_DC);
+static void (*zend_do_begin_new_object_t) = zend_do_begin_new_object;
+void php_classnameresolver_do_begin_new_object(znode *new_token, znode *class_type TSRMLS_DC); 
+static zend_class_entry *classnameresolver_ce;
 
 #ifdef ZTS
 #define CLASSNAMERESOLVER(v) TSRMG(classnameresolver_globals_id, zend_classnameresolver_globals *, v)
@@ -22,12 +29,15 @@ PHP_MINFO_FUNCTION(classnameresolver);
 #define CLASSNAMERESOLVER(v) (classnameresolver_globals.v)
 #endif
 
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
 #ifndef TRUE
 #define TRUE 1
 #define FALSE 0
 #endif
 
-#define CLASSNAMERESOLVER_EXT_VERSION "0.0.1"
 
 #endif
 
